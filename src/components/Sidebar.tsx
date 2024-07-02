5b18e860ad4bfda9b5c83a8b636669569975e355
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-07-02 16:24:55 +0100

    active-link

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,20 +1,17 @@
+"use client";
+import React from "react";
 import Link from "next/link";
 import Image from "next/image";
+import { usePathname } from "next/navigation";
 import profile from "../images/Avatar.png";
 import { ChevronsUpDown } from "lucide-react";
-
-import {
-	
-	Home,
-	LineChart,
-
-	Package,
-
-	ShoppingCart,
-	Users,
-} from "lucide-react";
-
-import { Badge } from "@/components/ui/badge";
+import DashboardIcon from "../images/dashboard.svg";
+import JournalIcon from "../images/journal.svg";
+import ArrowIcon from "../images/arrow.svg";
+import MergeIcon from "../images/merge.svg";
+import DownArrowIcon from "../images/arrow-big-down-dash.svg";
+import TransactionIcon from "../images/transaction.svg";
+import WalletIcon from "../images/wallet.svg";
 import apexIcon from "../images/Apex.svg";
 import databaseIcon from "../images/database.svg";
 
@@ -27,22 +24,20 @@ import {
 	DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { ComboboxDemo } from "./Combobox";
-import DashboardIcon from "../images/dashboard.svg";
-import JournalIcon from "../images/journal.svg";
-import ArrowIcon from "../images/arrow.svg";
-import MergeIcon from "../images/merge.svg";
-import DownArrowIcon from "../images/arrow-big-down-dash.svg";
-import TransactionIcon from "../images/transaction.svg";
-import WalletIcon from "../images/wallet.svg";
+
 export function Sidebar() {
+	const pathname = usePathname();
+
+	const isActive = (path:string) => pathname === path;
+
 	return (
 		<div
 			className="hidden md:grid md:grid-cols-[220px_1fr] lg:grid-cols-[225px_1fr] min-h-screen"
 			style={{ backgroundColor: "#FAFAFA" }}
 		>
-			<div className="border-r " style={{ border: "#FAFAFA" }}>
+			<div className="border-r" style={{ borderColor: "#F4F4F5" }}>
 				<div className="flex h-full max-h-screen flex-col">
-					<div className="flex h-14 items-center  px-4 lg:h-[60px] lg:px-6 gap-3">
+					<div className="flex h-14 items-center px-4 lg:h-[60px] lg:px-6 gap-3">
 						<div className="avatar rounded-full flex items-center justify-around gap-12">
 							<div className="flex items-center gap-2">
 								<Image src={profile} alt="user-pics" width={22} height={22} />
@@ -68,10 +63,12 @@ export function Sidebar() {
 						</div>
 					</div>
 					<div className="flex-1">
-						<nav className="grid items-start px-2  text-sm font-medium lg:px-4">
+						<nav className="grid items-start px-2 text-sm font-medium lg:px-4">
 							<Link
 								href="/"
-								className="flex items-center gap-3  rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/") ? " bg-[#F4F4F5]" : ""
+								}`}
 							>
 								<Image
 									src={DashboardIcon}
@@ -82,17 +79,22 @@ export function Sidebar() {
 							</Link>
 							<Link
 								href="/dashboard/wallet"
-								className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/dashboard/wallet")
+										? " bg-[#F4F4F5]"
+										: ""
+								}`}
 							>
 								<Image src={WalletIcon} alt="wallet" className="h-4 w-4" />
 								Wallet
-								{/* <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">
-									6
-								</Badge> */}
 							</Link>
 							<Link
 								href="/dashboard/transactions"
-								className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/dashboard/transactions")
+										? " bg-[#F4F4F5]"
+										: ""
+								}`}
 							>
 								<Image
 									src={TransactionIcon}
@@ -103,14 +105,22 @@ export function Sidebar() {
 							</Link>
 							<Link
 								href="/dashboard/reversals"
-								className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/dashboard/reversals")
+										? " bg-[#F4F4F5]"
+										: ""
+								}`}
 							>
-								<Image src={MergeIcon} alt="reverasls" className="h-4 w-4" />
+								<Image src={MergeIcon} alt="reversals" className="h-4 w-4" />
 								Reversals
 							</Link>
 							<Link
 								href="/dashboard/wallet-funding"
-								className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/dashboard/wallet-funding")
+										? " bg-[#F4F4F5]"
+										: ""
+								}`}
 							>
 								<Image
 									src={DownArrowIcon}
@@ -121,7 +131,11 @@ export function Sidebar() {
 							</Link>
 							<Link
 								href="/dashboard/outflow-transfer"
-								className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/dashboard/outflow-transfer")
+										? " bg-[#F4F4F5]"
+										: ""
+								}`}
 							>
 								<Image
 									src={ArrowIcon}
@@ -132,7 +146,11 @@ export function Sidebar() {
 							</Link>
 							<Link
 								href="/dashboard/journal-entries"
-								className="flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary"
+								className={`flex items-center gap-3 rounded-lg px-3 py-3 text-muted-foreground transition-all hover:text-primary ${
+									isActive("/dashboard/journal-entries")
+										? " bg-[#F4F4F5]"
+										: ""
+								}`}
 							>
 								<Image
 									src={JournalIcon}
@@ -143,7 +161,7 @@ export function Sidebar() {
 							</Link>
 						</nav>
 					</div>
-					<div className="mt-auto lg:w-[224px] md:w-[224px] w-[224px] h-[70px] px-2 py-1.5 flex items-center justify-between border-t-0 border-b-0  bg-white">
+					<div className="mt-auto lg:w-[224px] md:w-[224px] w-[224px] h-[70px] px-2 py-1.5 flex items-center justify-between border-t-0 border-b-0 bg-white">
 						<div className="icon relative flex items-center justify-center">
 							<Image src={apexIcon} alt="icon" className="w-full h-full" />
 							<Image
@@ -166,15 +184,6 @@ export function Sidebar() {
 					</div>
 				</div>
 			</div>
-			{/* <div className="flex-1 flex flex-col">
-        <Header />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
-          <div className="flex items-center">
-            <h1 className="text-lg font-semibold md:text-2xl">Orders</h1>
-          </div>
-          Page content
-        </main>
-      </div> */}
 		</div>
 	);
 }
