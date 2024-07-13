54a95c804fc88b0dbda83fc67f3e840978e1b81c
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-07-13 11:02:08 +0100

    deployment-resolved

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,52 +1,13 @@
-// "use client";
-// import React, { useState } from "react";
-// import circle from "../images/circle.png";
-// import Person from "../images/person.svg";
-// import Image from "next/image";
+import { EmailTable } from "@/components/EmailTable";
+import React from "react";
 
-// const Page = () => {
-// 	const [data, setData] = useState(false);
+const Emails = () => {
+	// const mailId =100
+	return (
+		<div className="m-4">
+			<EmailTable />
+		</div>
+	);
+};
 
-// 	return (
-// 		<div className="flex justify-center">
-// 			<div className="w-full max-w-7xl p-6 mt-3 ">
-// 				{!data ? (
-// 					<div
-// 						className="flex flex-1 items-center justify-center rounded-lg border border-dashed shadow-sm w-full h-[462px] bg-white"
-// 						style={{
-// 							height: "462px",
-// 							borderRadius: "5px",
-// 							borderColor: "#dadada",
-// 						}}
-// 					>
-// 						<div className="flex flex-col items-center gap-0 text-center mt-[-160px]">
-// 							<div className="empty-icons">
-// 								<div className="flex items-center justify-center ">
-// 									<div className="relative">
-// 										<Image src={circle} alt="empty pos" />
-// 										<div className="absolute inset-0 flex items-center justify-center">
-// 											<Image src={Person} alt="person" />
-// 										</div>
-// 									</div>
-// 								</div>
-// 							</div>
-// 							<div className="no-data leading-[32px]">
-// 								<p className="first-text text-[15px] font-semibold text-[#3F3F46]">
-// 									No POS terminals assigned yet
-// 								</p>
-// 								<p className="second-text leading-[20px] text-[13px] text-[#71717A]">
-// 									All assigned POS terminals will appear here. Go <br />
-// 									and assign one
-// 								</p>
-// 							</div>
-// 						</div>
-// 					</div>
-// 				) : (
-// 					<p>Data is available</p>
-// 				)}
-// 			</div>
-// 		</div>
-// 	);
-// };
-
-// export default Page;
+export default Emails;
