4c39d4eaeb67c7e9c15e13860c08d9e41c2eac5e
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-07-22 16:23:39 +0100

    add-slider

--- src/components/settings_page/usage/TransactionalContent.tsx
+++ src/components/settings_page/usage/TransactionalContent.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Slider } from "@/components/ui/slider";
 
 export const TransactionalContent = () => {
   return (
@@ -22,7 +23,7 @@ export const TransactionalContent = () => {
 									</span>
 								</span>
 							</span>
-							<hr className="border-gray-300" />
+							<hr className="border-gray-300 mb-4" />
 							<ul className="flex flex-col gap-3">
 								<li className="flex items-center gap-2 text-left">
 									<svg
@@ -60,81 +61,80 @@ export const TransactionalContent = () => {
 										1 Domain
 									</span>
 								</li>
-							
 
-							<li className="flex items-center gap-2 text-left">
-								<svg
-									className="text-green-600"
-									fill="none"
-									height="24"
-									viewBox="0 0 24 24"
-									width="24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM15.5312 10.5312L11.5312 14.5312C11.2375 14.825 10.7625 14.825 10.4719 14.5312L8.47188 12.5312C8.17813 12.2375 8.17813 11.7625 8.47188 11.4719C8.76563 11.1813 9.24062 11.1781 9.53125 11.4719L11 12.9406L14.4688 9.46875C14.7625 9.175 15.2375 9.175 15.5281 9.46875C15.8187 9.7625 15.8219 10.2375 15.5281 10.5281L15.5312 10.5312Z"
-										fill="currentColor"
-									></path>
-								</svg>
-								<span className="font-sans text-sm leading-6 text-slate-11 font-normal">
-									1-day Data Retention
-								</span>
-							</li>
-							<li className="flex items-center gap-2 text-left">
-								<svg
-									className="text-gray-500"
-									fill="none"
-									height="24"
-									viewBox="0 0 24 24"
-									width="24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM9.46875 9.46875C9.7625 9.175 10.2375 9.175 10.5281 9.46875L11.9969 10.9375L13.4656 9.46875C13.7594 9.175 14.2344 9.175 14.525 9.46875C14.8156 9.7625 14.8187 10.2375 14.525 10.5281L13.0562 11.9969L14.525 13.4656C14.8187 13.7594 14.8187 14.2344 14.525 14.525C14.2312 14.8156 13.7562 14.8187 13.4656 14.525L11.9969 13.0562L10.5281 14.525C10.2344 14.8187 9.75938 14.8187 9.46875 14.525C9.17812 14.2312 9.175 13.7562 9.46875 13.4656L10.9375 11.9969L9.46875 10.5281C9.175 10.2344 9.175 9.75938 9.46875 9.46875Z"
-										fill="currentColor"
-									></path>
-								</svg>
-								<span className="sans text-sm leading-[1.6] text-slate-11 font-normal">
-									Multi-Region
-								</span>
-							</li>
-							<li className="flex items-center gap-2 text-left">
-								<svg
-									className="text-gray-500"
-									fill="none"
-									height="24"
-									viewBox="0 0 24 24"
-									width="24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM9.46875 9.46875C9.7625 9.175 10.2375 9.175 10.5281 9.46875L11.9969 10.9375L13.4656 9.46875C13.7594 9.175 14.2344 9.175 14.525 9.46875C14.8156 9.7625 14.8187 10.2375 14.525 10.5281L13.0562 11.9969L14.525 13.4656C14.8187 13.7594 14.8187 14.2344 14.525 14.525C14.2312 14.8156 13.7562 14.8187 13.4656 14.525L11.9969 13.0562L10.5281 14.525C10.2344 14.8187 9.75938 14.8187 9.46875 14.525C9.17812 14.2312 9.175 13.7562 9.46875 13.4656L10.9375 11.9969L9.46875 10.5281C9.175 10.2344 9.175 9.75938 9.46875 9.46875Z"
-										fill="currentColor"
-									></path>
-								</svg>
-								<span className="sans text-sm leading-[1.6] text-slate-11 font-normal">
-									Single Sign-On
-								</span>
-							</li>
-							<li className="flex items-center gap-2 text-left">
-								<svg
-									className="text-gray-500"
-									fill="none"
-									height="24"
-									viewBox="0 0 24 24"
-									width="24"
-									xmlns="http://www.w3.org/2000/svg"
-								>
-									<path
-										d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM9.46875 9.46875C9.7625 9.175 10.2375 9.175 10.5281 9.46875L11.9969 10.9375L13.4656 9.46875C13.7594 9.175 14.2344 9.175 14.525 9.46875C14.8156 9.7625 14.8187 10.2375 14.525 10.5281L13.0562 11.9969L14.525 13.4656C14.8187 13.7594 14.8187 14.2344 14.525 14.525C14.2312 14.8156 13.7562 14.8187 13.4656 14.525L11.9969 13.0562L10.5281 14.525C10.2344 14.8187 9.75938 14.8187 9.46875 14.525C9.17812 14.2312 9.175 13.7562 9.46875 13.4656L10.9375 11.9969L9.46875 10.5281C9.175 10.2344 9.175 9.75938 9.46875 9.46875Z"
-										fill="currentColor"
-									></path>
-								</svg>
-								<span className="sans text-sm leading-[1.6] text-slate-11 font-normal">
-									Dedicated IPs
-								</span>
-							</li>
-</ul>
+								<li className="flex items-center gap-2 text-left">
+									<svg
+										className="text-green-600"
+										fill="none"
+										height="24"
+										viewBox="0 0 24 24"
+										width="24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM15.5312 10.5312L11.5312 14.5312C11.2375 14.825 10.7625 14.825 10.4719 14.5312L8.47188 12.5312C8.17813 12.2375 8.17813 11.7625 8.47188 11.4719C8.76563 11.1813 9.24062 11.1781 9.53125 11.4719L11 12.9406L14.4688 9.46875C14.7625 9.175 15.2375 9.175 15.5281 9.46875C15.8187 9.7625 15.8219 10.2375 15.5281 10.5281L15.5312 10.5312Z"
+											fill="currentColor"
+										></path>
+									</svg>
+									<span className="font-sans text-sm leading-6 text-slate-11 font-normal">
+										1-day Data Retention
+									</span>
+								</li>
+								<li className="flex items-center gap-2 text-left">
+									<svg
+										className="text-gray-500"
+										fill="none"
+										height="24"
+										viewBox="0 0 24 24"
+										width="24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM9.46875 9.46875C9.7625 9.175 10.2375 9.175 10.5281 9.46875L11.9969 10.9375L13.4656 9.46875C13.7594 9.175 14.2344 9.175 14.525 9.46875C14.8156 9.7625 14.8187 10.2375 14.525 10.5281L13.0562 11.9969L14.525 13.4656C14.8187 13.7594 14.8187 14.2344 14.525 14.525C14.2312 14.8156 13.7562 14.8187 13.4656 14.525L11.9969 13.0562L10.5281 14.525C10.2344 14.8187 9.75938 14.8187 9.46875 14.525C9.17812 14.2312 9.175 13.7562 9.46875 13.4656L10.9375 11.9969L9.46875 10.5281C9.175 10.2344 9.175 9.75938 9.46875 9.46875Z"
+											fill="currentColor"
+										></path>
+									</svg>
+									<span className="sans text-sm leading-[1.6] text-slate-11 font-normal">
+										Multi-Region
+									</span>
+								</li>
+								<li className="flex items-center gap-2 text-left">
+									<svg
+										className="text-gray-500"
+										fill="none"
+										height="24"
+										viewBox="0 0 24 24"
+										width="24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM9.46875 9.46875C9.7625 9.175 10.2375 9.175 10.5281 9.46875L11.9969 10.9375L13.4656 9.46875C13.7594 9.175 14.2344 9.175 14.525 9.46875C14.8156 9.7625 14.8187 10.2375 14.525 10.5281L13.0562 11.9969L14.525 13.4656C14.8187 13.7594 14.8187 14.2344 14.525 14.525C14.2312 14.8156 13.7562 14.8187 13.4656 14.525L11.9969 13.0562L10.5281 14.525C10.2344 14.8187 9.75938 14.8187 9.46875 14.525C9.17812 14.2312 9.175 13.7562 9.46875 13.4656L10.9375 11.9969L9.46875 10.5281C9.175 10.2344 9.175 9.75938 9.46875 9.46875Z"
+											fill="currentColor"
+										></path>
+									</svg>
+									<span className="sans text-sm leading-[1.6] text-slate-11 font-normal">
+										Single Sign-On
+									</span>
+								</li>
+								<li className="flex items-center gap-2 text-left">
+									<svg
+										className="text-gray-500"
+										fill="none"
+										height="24"
+										viewBox="0 0 24 24"
+										width="24"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M12 20C16.4187 20 20 16.4187 20 12C20 7.58125 16.4187 4 12 4C7.58125 4 4 7.58125 4 12C4 16.4187 7.58125 20 12 20ZM9.46875 9.46875C9.7625 9.175 10.2375 9.175 10.5281 9.46875L11.9969 10.9375L13.4656 9.46875C13.7594 9.175 14.2344 9.175 14.525 9.46875C14.8156 9.7625 14.8187 10.2375 14.525 10.5281L13.0562 11.9969L14.525 13.4656C14.8187 13.7594 14.8187 14.2344 14.525 14.525C14.2312 14.8156 13.7562 14.8187 13.4656 14.525L11.9969 13.0562L10.5281 14.525C10.2344 14.8187 9.75938 14.8187 9.46875 14.525C9.17812 14.2312 9.175 13.7562 9.46875 13.4656L10.9375 11.9969L9.46875 10.5281C9.175 10.2344 9.175 9.75938 9.46875 9.46875Z"
+											fill="currentColor"
+										></path>
+									</svg>
+									<span className="sans text-sm leading-[1.6] text-slate-11 font-normal">
+										Dedicated IPs
+									</span>
+								</li>
+							</ul>
 							<div>
 								<button className="text-sm h-8 pl-3 pr-3 rounded-md gap-1 font-semibold bg-black dark:bg-white text-white dark:text-black border-slate-6 hover:bg-black/90 dark:hover:bg-white/90 focus-visible:ring-2 dark:focus-visible:ring-white/40 focus-visible:ring-black/40 focus-visible:outline-none dark:focus-visible:bg-white/90 focus-visible:bg-black/90 disabled:hover:bg-black dark:disabled:hover:bg-white inline-flex items-center justify-center border select-none relative disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 cursor-pointer mt-6 block w-full">
 									<span className="absolute w-full inset-0 flex items-center justify-center invisible">
@@ -173,52 +173,18 @@ export const TransactionalContent = () => {
 								$20 / mo
 							</span>
 							<hr className="border-gray-300" />
-							<span
-								dir="ltr"
-								data-orientation="horizontal"
-								aria-disabled="false"
-								aria-label="Volume"
-								className="relative mb-2.5 mt-4 flex h-5 w-full touch-none items-center"
-							>
-								<span
-									data-orientation="horizontal"
-									className="relative h-1 w-full grow rounded-full bg-slate-5"
-								>
-									<span
-										data-orientation="horizontal"
-										className="absolute h-full rounded-full bg-gradient-to-r from-slate-12 to-slate-11"
-										style={{ left: "0%", right: "79.0323%" }}
-									></span>
-								</span>
-								<span
-									style={{
-										transform: "var(--radix-slider-thumb-transform)",
-										position: "absolute",
-										left: "calc(20.9677% + 5.80645px)",
-									}}
-								>
-									<span
-										role="slider"
-										aria-valuemin={0}
-										aria-valuemax={62}
-										aria-orientation="horizontal"
-										data-orientation="horizontal"
-										tabIndex={0}
-										className="block h-5 w-5 cursor-grab rounded-full bg-black bg-gradient-to-r from-slate-12 to-slate-11 focus-visible:outline-none focus-visible:ring focus-visible:ring-slate-7 focus-visible:ring-opacity-75 active:cursor-grabbing dark:bg-white"
-										data-radix-collection-item=""
-										aria-valuenow={13}
-									></span>
-								</span>
+							<span className="font-sans my-5 text-center text-sm leading-4 text-slate-12 font-normal">
+								<Slider className="" defaultValue={[33]} max={100} step={1} />
 							</span>
 
-							<span className="sans mb-4 text-center text-sm leading-[1.6] text-slate-12 font-normal">
+							<span className="sans mb-5 text-center text-sm leading-[1.6] text-slate-12 font-normal">
 								50,000 emails / mo
 								<span className="mt-2 block text-slate-11">
 									No daily sending limit
 								</span>
 							</span>
-							<hr className="border-gray-300" />
-							<ul className="flex flex-col gap-3">
+							<hr className="border-gray-300 mb-4" />
+							<ul className="flex flex-col gap-3 ">
 								<li className="flex items-center gap-2 text-left">
 									<svg
 										className="text-green-600"
@@ -361,7 +327,7 @@ export const TransactionalContent = () => {
 									No daily sending limit
 								</span>
 							</span>
-							<hr className="border-gray-300" />
+							<hr className="border-gray-300 mb-4" />
 							<ul className="flex flex-col gap-3">
 								<li className="flex items-center gap-2 text-left">
 									<svg
