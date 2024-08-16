e8f3daf7c6fbf6113eb02f15029d772cfe1849ac
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-08-16 16:47:30 +0100

    delete

--- src/app/dashboard/settings/team/page.tsx
+++ src/app/dashboard/settings/team/page.tsx
@@ -42,6 +42,20 @@ const TeamSettings=()=> {
 
 					<MemberTable />
 				</section>
+
+			<section className="rounded-lg border border-gray-300 mt-6 mb-16">
+				<div className="border-b border-slate-6 p-4">
+					<h1 className="text-base text-slate-12 font-bold">Account</h1>
+					</div>
+				<div className="flex flex-col items-start gap-4  p-4">
+					<p className="text-sm text-gray-500 font-normal">Permanently remove your account and all of its contents from Resend.
+					<br />
+					This action is not reversible, so please continue with caution.</p>
+					<Button type="button"  className="font-medium inline-flex items-center justify-center border select-none relative cursor-pointer disabled:cursor-not-allowed disabled:opacity-70 transition ease-in-out duration-200 bg-red-100 border-red-100  text-red-600 hover:bg-red-200 focus-visible:ring-2 focus-visible:ring-red-300 focus-visible:outline-none focus-visible:bg-red-200 disabled:hover:bg-red-4 text-sm h-8 pl-3 pr-3 rounded-md gap-1">
+					<span className="inline-flex items-center justify-center gap-1 truncate visible">Request Account Deletion</span>
+					</Button>
+					</div>
+					</section>
 				</div>
 				</SettingsLayout>
 	);
