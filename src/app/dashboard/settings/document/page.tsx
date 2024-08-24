c52c74c45f0afb40b768ad8a1a50267be1cbafdc
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-08-24 11:15:55 +0100

    document

--- src/app/dashboard/settings/document/page.tsx
+++ src/app/dashboard/settings/document/page.tsx
@@ -15,27 +15,82 @@ const DocumentSettings =()=> {
 	return (		
 	<>
 	<SettingsLayout >
-	<Card>
-		<CardHeader>
-			<CardTitle>Document</CardTitle>
-			<CardDescription>
-				Make changes to your account here. Click save when you're done.
-			</CardDescription>
-		</CardHeader>
-		<CardContent className="space-y-2">
-			<div className="space-y-1">
-				<Label htmlFor="name">Name</Label>
-				<Input id="name" defaultValue="Pedro Duarte" />
-			</div>
-			<div className="space-y-1">
-				<Label htmlFor="username">Username</Label>
-				<Input id="username" defaultValue="@peduarte" />
-			</div>
-		</CardContent>
-		<CardFooter>
-			<Button>Save changes</Button>
-		</CardFooter>
-	</Card>
+		 <div className="mr-40">
+<section className="rounded-lg border border-gray-300 my-5">
+          <div className="border-b border-slate-6 px-6 py-4">
+            <h2 className="text-xl tracking-[-0.16px] text-slate-12 font-bold">
+              Invite member
+            </h2>
+          </div>
+          <div className="mb-2 max-w-md px-6 py-4 gap-2 flex flex-col">
+            <label htmlFor="email" className="text-sm text-gray-500">
+              Email Address
+            </label>
+            <Input
+              className="border-slate-6 bg-gray-100 border-gray-300 text-gray-500 focus-visible:ring-slate-700 transition ease-in-out duration-200 placeholder:text-gray-500 h-8 rounded-md px-2 text-sm outline-none focus-visible:ring-2"
+              type="email"
+              id="email"
+              placeholder="you@example.com"
+              value=""
+            />
+          </div>
+          
+            <Button  className="bg-zinc-700 h-8">
+              Invite
+            </Button>
+     
+        </section>
+
+				<section className="rounded-lg border border-gray-300 my-5">
+          <div className="border-b border-slate-6 px-6 py-4">
+            <h2 className="text-xl tracking-[-0.16px] text-slate-12 font-bold">
+              Invite member
+            </h2>
+          </div>
+          <div className="mb-2 max-w-md px-6 py-4 gap-2 flex flex-col">
+            <label htmlFor="email" className="text-sm text-gray-500">
+              Email Address
+            </label>
+            <Input
+              className="border-slate-6 bg-gray-100 border-gray-300 text-gray-500 focus-visible:ring-slate-700 transition ease-in-out duration-200 placeholder:text-gray-500 h-8 rounded-md px-2 text-sm outline-none focus-visible:ring-2"
+              type="email"
+              id="email"
+              placeholder="you@example.com"
+              value=""
+            />
+          </div>
+          
+            <Button  className="bg-zinc-700 h-8">
+              Invite
+            </Button>
+     
+        </section>
+
+				<section className="rounded-lg border border-gray-300 my-5">
+          <div className="border-b border-slate-6 px-6 py-4">
+            <h2 className="text-xl tracking-[-0.16px] text-slate-12 font-bold">
+              Invite member
+            </h2>
+          </div>
+          <div className="mb-2 max-w-md px-6 py-4 gap-2 flex flex-col">
+            <label htmlFor="email" className="text-sm text-gray-500">
+              Email Address
+            </label>
+            <Input
+              className="border-slate-6 bg-gray-100 border-gray-300 text-gray-500 focus-visible:ring-slate-700 transition ease-in-out duration-200 placeholder:text-gray-500 h-8 rounded-md px-2 text-sm outline-none focus-visible:ring-2"
+              type="email"
+              id="email"
+              placeholder="you@example.com"
+              value=""
+            />
+          </div>
+          
+            <Button  className="bg-zinc-700 h-8">
+              Invite
+            </Button>
+     
+        </section>
+				</div>
 	</SettingsLayout>
 	</>
 	);
