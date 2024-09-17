080d4d6ed88e4cff710a61698b742cfc079c6a4b
Chinwendu Enyidiegwu <chinwe640@gmail.com>
2024-09-17 15:49:45 +0100

    pushed

--- src/app/dashboard/api-keys/[api-keys]/page.tsx
+++ src/app/dashboard/api-keys/[api-keys]/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
  const ApiKeysId = () => {
   return (
-    <><div className='m-6'><Button>apiyy</Button></div></>
+    <><div className='m-6'><Button>ap</Button></div></>
   )
 }
 export default ApiKeysId;
